5dec28f584ecb994311711ab3d648999954fd750
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2023-11-17 16:40:43 +0300

    Coordinate validation

--- src/elevation/elevation.rs
+++ src/elevation/elevation.rs
@@ -3,29 +3,31 @@ use nav_types::WGS84;
 use crate::errors::Error;
 use crate::tile_storage::tile_signature::TileSignature;
 use crate::tile_storage::tile_storage::STORAGE;
+use crate::utils::validate_coordinate;
 
 pub fn elevation_at(coordinate: (f64, f64)) -> Result<f32, Error>
 {
   let mut storage = STORAGE
     .lock()
     .unwrap();
-  let key = TileSignature::from_f64(coordinate.0, coordinate.1);
+  let coord = validate_coordinate(coordinate)?;
+  let key = TileSignature::from_f64(coord.0, coord.1);
   let image_size = storage.get_or_emplace(key)?.size;
   let data = &storage.get_or_emplace(key)?.data;
   let tile_size = key.georectangle_size();
-  let requested_coordinate = WGS84::from_degrees_and_meters(coordinate.0,
-                                                            coordinate.1, 0.0);
-  let distance_2d = (requested_coordinate.distance(&WGS84::from_degrees_and_meters(coordinate.0,
+  let requested_coordinate = WGS84::from_degrees_and_meters(coord.0,
+                                                            coord.1, 0.0);
+  let distance_2d = (requested_coordinate.distance(&WGS84::from_degrees_and_meters(coord.0,
                                                                                    key.longitude
                                                                                      as f64, 0.0)),
                                  requested_coordinate.distance(&WGS84::from_degrees_and_meters(key.latitude
                                                                                      as f64,
-                                                                                   coordinate.1,
+                                                                                               coord.1,
                                                                                    0.0)));
   let dn = (distance_2d.0 / (tile_size.1 as f64), distance_2d.1 / (tile_size.0 as f64));
   let pixel_coords = ((dn.0 * image_size.0 as f64) as usize, (dn.1 * image_size.1 as f64) as usize);
   let value = data.get_pixel(pixel_coords.1, pixel_coords.0);
-  debug!("Elevation at {:?} is {} meters", coordinate, value);
+  debug!("Elevation at {:?} is {} meters", coord, value);
 
   Ok(value as f32)
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,21 @@
 use std::sync::Mutex;
 use once_cell::sync::Lazy;
+use crate::errors::Error;
 
 pub type StaticHeapObject<T> = Lazy<Mutex<Box<T>>>;
+
+pub fn validate_coordinate(coordinate: (f64, f64)) -> Result<(f64, f64), Error>
+{
+  if coordinate.0 < -90.0 || coordinate.0 > 90.0 || coordinate.1 < -180.0 || coordinate.1 > 180.0 {
+    return Err(Error::InvalidCoordinate);
+  }
+  const THRESHOLD: f64 = 0.00001;
+  let mut lat = coordinate.0;
+  let mut lon = coordinate.1;
+  if coordinate.0 - coordinate.0.floor() < THRESHOLD { lat = coordinate.0.floor(); }
+  if coordinate.0.ceil() - coordinate.0 < THRESHOLD { lat = coordinate.0.ceil(); }
+  if coordinate.1 - coordinate.1.floor() < THRESHOLD { lon = coordinate.1.floor(); }
+  if coordinate.1.ceil() - coordinate.1 < THRESHOLD { lon = coordinate.1.ceil(); }
+
+  return Ok((lat, lon));
+}
