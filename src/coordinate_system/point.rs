3746e6e31bc48d611e0ec2d1991697e236e50427
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2023-11-26 16:09:16 +0300

    Update point.rs

--- src/coordinate_system/point.rs
+++ src/coordinate_system/point.rs
@@ -52,62 +52,74 @@ impl CoordinatePoint
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn test_web_mercator()
-    {
-      let lat_lon = GeoCoordinate::new(41.85, -87.65, None);
-      let point = CoordinatePoint::from(lat_lon);
+  #[test]
+  fn test_web_mercator_world_coordinate()
+  {
+    let lat_lon = GeoCoordinate::new(41.85, -87.65, None);
+    let point = CoordinatePoint::from(lat_lon);
 
-      assert_eq!(point.world_coordinate(), (65.67111111111112, 95.17492654697409));
+    assert_eq!(point.world_coordinate(), (65.67111111111112, 95.17492654697409));
+  }
 
-      assert_eq!(point.pixel_coordinate(0), (65, 95));
-      assert_eq!(point.pixel_coordinate(1), (131, 190));
-      assert_eq!(point.pixel_coordinate(2), (262, 380));
-      assert_eq!(point.pixel_coordinate(3), (525, 761));
-      assert_eq!(point.pixel_coordinate(4), (1050, 1522));
-      assert_eq!(point.pixel_coordinate(5), (2101, 3045));
-      assert_eq!(point.pixel_coordinate(6), (4202, 6091));
-      assert_eq!(point.pixel_coordinate(7), (8405, 12182));
-      assert_eq!(point.pixel_coordinate(8), (16811, 24364));
-      assert_eq!(point.pixel_coordinate(9), (33623, 48729));
-      assert_eq!(point.pixel_coordinate(10), (67247, 97459));
-      assert_eq!(point.pixel_coordinate(11), (134494, 194918));
-      assert_eq!(point.pixel_coordinate(12), (268988, 389836));
-      assert_eq!(point.pixel_coordinate(13), (537977, 779672));
-      assert_eq!(point.pixel_coordinate(14), (1075955, 1559345));
-      assert_eq!(point.pixel_coordinate(15), (2151910, 3118691));
-      assert_eq!(point.pixel_coordinate(16), (4303821, 6237383));
-      assert_eq!(point.pixel_coordinate(17), (8607643, 12474767));
-      assert_eq!(point.pixel_coordinate(18), (17215287, 24949535));
-      assert_eq!(point.pixel_coordinate(19), (34430575, 49899071));
-      assert_eq!(point.pixel_coordinate(20), (68861151, 99798143));
-      assert_eq!(point.pixel_coordinate(21), (137722302, 199596287));
-      assert_eq!(point.pixel_coordinate(22), (275444604, 399192575));
+  #[test]
+  fn test_web_mercator_pixel_coordinate() {
+    let lat_lon = GeoCoordinate::new(41.85, -87.65, None);
+    let point = CoordinatePoint::from(lat_lon);
 
-      assert_eq!(point.tile_coordinate(0), (0, 0));
-      assert_eq!(point.tile_coordinate(1), (0, 0));
-      assert_eq!(point.tile_coordinate(2), (1, 1));
-      assert_eq!(point.tile_coordinate(3), (2, 2));
-      assert_eq!(point.tile_coordinate(4), (4, 5));
-      assert_eq!(point.tile_coordinate(5), (8, 11));
-      assert_eq!(point.tile_coordinate(6), (16, 23));
-      assert_eq!(point.tile_coordinate(7), (32, 47));
-      assert_eq!(point.tile_coordinate(8), (65, 95));
-      assert_eq!(point.tile_coordinate(9), (131, 190));
-      assert_eq!(point.tile_coordinate(10), (262, 380));
-      assert_eq!(point.tile_coordinate(11), (525, 761));
-      assert_eq!(point.tile_coordinate(12), (1050, 1522));
-      assert_eq!(point.tile_coordinate(13), (2101, 3045));
-      assert_eq!(point.tile_coordinate(14), (4202, 6091));
-      assert_eq!(point.tile_coordinate(15), (8405, 12182));
-      assert_eq!(point.tile_coordinate(16), (16811, 24364));
-      assert_eq!(point.tile_coordinate(17), (33623, 48729));
-      assert_eq!(point.tile_coordinate(18), (67247, 97459));
-      assert_eq!(point.tile_coordinate(19), (134494, 194918));
-      assert_eq!(point.tile_coordinate(20), (268988, 389836));
-      assert_eq!(point.tile_coordinate(21), (537977, 779672));
-      assert_eq!(point.tile_coordinate(22), (1075955, 1559345));
-    }
+    assert_eq!(point.pixel_coordinate(0), (65, 95));
+    assert_eq!(point.pixel_coordinate(1), (131, 190));
+    assert_eq!(point.pixel_coordinate(2), (262, 380));
+    assert_eq!(point.pixel_coordinate(3), (525, 761));
+    assert_eq!(point.pixel_coordinate(4), (1050, 1522));
+    assert_eq!(point.pixel_coordinate(5), (2101, 3045));
+    assert_eq!(point.pixel_coordinate(6), (4202, 6091));
+    assert_eq!(point.pixel_coordinate(7), (8405, 12182));
+    assert_eq!(point.pixel_coordinate(8), (16811, 24364));
+    assert_eq!(point.pixel_coordinate(9), (33623, 48729));
+    assert_eq!(point.pixel_coordinate(10), (67247, 97459));
+    assert_eq!(point.pixel_coordinate(11), (134494, 194918));
+    assert_eq!(point.pixel_coordinate(12), (268988, 389836));
+    assert_eq!(point.pixel_coordinate(13), (537977, 779672));
+    assert_eq!(point.pixel_coordinate(14), (1075955, 1559345));
+    assert_eq!(point.pixel_coordinate(15), (2151910, 3118691));
+    assert_eq!(point.pixel_coordinate(16), (4303821, 6237383));
+    assert_eq!(point.pixel_coordinate(17), (8607643, 12474767));
+    assert_eq!(point.pixel_coordinate(18), (17215287, 24949535));
+    assert_eq!(point.pixel_coordinate(19), (34430575, 49899071));
+    assert_eq!(point.pixel_coordinate(20), (68861151, 99798143));
+    assert_eq!(point.pixel_coordinate(21), (137722302, 199596287));
+    assert_eq!(point.pixel_coordinate(22), (275444604, 399192575));
+  }
+
+  #[test]
+  fn test_web_mercator_tile_coordinate() {
+    let lat_lon = GeoCoordinate::new(41.85, -87.65, None);
+    let point = CoordinatePoint::from(lat_lon);
+
+    assert_eq!(point.tile_coordinate(0), (0, 0));
+    assert_eq!(point.tile_coordinate(1), (0, 0));
+    assert_eq!(point.tile_coordinate(2), (1, 1));
+    assert_eq!(point.tile_coordinate(3), (2, 2));
+    assert_eq!(point.tile_coordinate(4), (4, 5));
+    assert_eq!(point.tile_coordinate(5), (8, 11));
+    assert_eq!(point.tile_coordinate(6), (16, 23));
+    assert_eq!(point.tile_coordinate(7), (32, 47));
+    assert_eq!(point.tile_coordinate(8), (65, 95));
+    assert_eq!(point.tile_coordinate(9), (131, 190));
+    assert_eq!(point.tile_coordinate(10), (262, 380));
+    assert_eq!(point.tile_coordinate(11), (525, 761));
+    assert_eq!(point.tile_coordinate(12), (1050, 1522));
+    assert_eq!(point.tile_coordinate(13), (2101, 3045));
+    assert_eq!(point.tile_coordinate(14), (4202, 6091));
+    assert_eq!(point.tile_coordinate(15), (8405, 12182));
+    assert_eq!(point.tile_coordinate(16), (16811, 24364));
+    assert_eq!(point.tile_coordinate(17), (33623, 48729));
+    assert_eq!(point.tile_coordinate(18), (67247, 97459));
+    assert_eq!(point.tile_coordinate(19), (134494, 194918));
+    assert_eq!(point.tile_coordinate(20), (268988, 389836));
+    assert_eq!(point.tile_coordinate(21), (537977, 779672));
+    assert_eq!(point.tile_coordinate(22), (1075955, 1559345));
+  }
 }
