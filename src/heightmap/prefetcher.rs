1cf4754bd935e806ce8d453a6a919e46d53a6634
whs31 <75864292+whs31@users.noreply.github.com>
2023-11-28 19:00:46 +0300

    Update prefetcher.rs

--- src/heightmap/prefetcher.rs
+++ src/heightmap/prefetcher.rs
@@ -1,18 +1,12 @@
-use std::collections::HashSet;
-use std::io::Write;
-use std::path::MAIN_SEPARATOR;
-use futures::{stream, StreamExt};
-use indicatif::{ProgressBar, ProgressStyle};
-use log::{error, warn};
 use meridian_positioning::errors::PositioningError;
 use meridian_positioning::GeoRectangle;
+use parallel_downloader::SourceTarget;
 use crate::errors::Error;
 use crate::tile_storage::TileSignature;
 
 #[derive(Debug)]
 pub struct ElevationPrefetcher
 {
-  client: reqwest::Client,
   server_url: String,
   storage_url: String,
   extension: String,
@@ -26,7 +20,6 @@ impl ElevationPrefetcher
   {
     ElevationPrefetcher
     {
-      client: reqwest::Client::new(),
       server_url,
       storage_url,
       extension,
@@ -46,51 +39,35 @@ impl ElevationPrefetcher
       ))
       .collect::<Vec<TileSignature>>();
 
-    let progress_bar = ProgressBar::new(signatures.len() as u64);
-    progress_bar.set_style(ProgressStyle::with_template(
-      "{wide_msg} {spinner:.green} [{bar:20.yellow/white}] {pos:10}/ {len:10} ({percent:3}%)")
-      .unwrap()
-      .progress_chars("█░░"));
-    progress_bar.set_message(format!("Downloading elevation tiles from {}", self.server_url.as_str()));
-    let bodies = stream::iter(signatures)
-      .enumerate()
-      .map(|(i, signature)| {
-        let url = signature.to_url_threadsafe(self.extension.as_str(), self.server_url.as_str());
-        let fs_path = signature.to_abs_path_threadsafe(self.extension.as_str(), self.storage_url.as_str());
-
-        let client = self.client.clone();
-        tokio::spawn(async move {
-          let response = client.get(&url).send().await?;
-          if !response.status().is_success() {
-            return Err(Error::NetworkStatusCodeErrorStr(response.status().as_u16(), url.clone()));
-          }
-
-          std::fs::create_dir_all(fs_path[..fs_path.rfind(MAIN_SEPARATOR).unwrap()]
-            .to_string()
-          )?;
-          let mut file = std::fs::File::create(&fs_path)?;
-          let mut stream = response.bytes_stream();
-          while let Some(item) = stream.next().await
-          {
-            let chunk = item?;
-            file.write_all(&chunk)?;
-          }
-          Ok(())
-        })
-      }).buffer_unordered(self.parallel_threads);
-
-    bodies
-      .enumerate()
-      .for_each(|(i, b)| async {
-        progress_bar.inc(1);
-        match b {
-          Err(e) => error!("Got a tokio::JoinError: {}", e),
-          _ => ()
+    let source_list: Vec<SourceTarget> = signatures
+      .iter()
+      .map(|s| {
+        let b = s.to_abs_path_threadsafe(
+          self.extension.as_str(),
+          self.storage_url.as_str()
+        );
+        let target = std::path::Path::new(b.as_str())
+          .parent()
+          .unwrap()
+          .to_str()
+          .unwrap();
+        SourceTarget
+        {
+          source: s.to_url_threadsafe(
+            self.extension.as_str(),
+            self.server_url.as_str()
+          ),
+          target: target.to_string()
         }
       })
-      .await;
-    progress_bar.finish_with_message("Elevation tiles downloaded");
-    println!();
+      .collect();
+
+    parallel_downloader::parallel_download(
+      source_list,
+      self.parallel_threads,
+      1,
+      10
+    ).await?;
     Ok(())
   }
 
